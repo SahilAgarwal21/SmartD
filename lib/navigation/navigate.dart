7d76da13d1a33641945e1f7d3af971fe0e9d3132
Sahil Agarwal <sahil11875@gmail.com>
2022-04-16 22:31:44 +0530

    test 1

--- lib/navigation/navigate.dart
+++ lib/navigation/navigate.dart
@@ -1,10 +1,11 @@
 import 'package:smart_d/screens/home_page.dart';
 import 'package:smart_d/screens/welcome_page.dart';
 import 'package:flutter/material.dart';
+import 'package:smart_d/message_list.dart';
 
 class Navigate {
   static Map<String, Widget Function(BuildContext)> routes =   {
     '/' : (context) => WelcomePage(),
-    '/home'  : (context) => HomePage()
+    '/home'  : (context) => MessageList()
   };
 }
